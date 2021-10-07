9a5dfe4b8c88fd61f51aaab894c10861c3c97dce
Ellwood12 <han16059@byui.edu>
2021-10-07 12:59:43 -0600

    Asher_Code

--- hilo/game/Dealer.py
+++ hilo/game/Dealer.py
@@ -1,16 +1,24 @@
+# %%
+import random
+
 class Dealer:
     def __init__(self):
         self.first_card = None
         self.second_card = None
 
     def First_card():
-        """
-        This is where we will create the first random card
-        """
-        pass
+        options = [i for i in range(13)]
+        return random.choice(options)
+
 
     def Second_card():
         """
         This is where we will create the second random card
         """
         pass
+
+# %%
+prac = Dealer.First_card()
+print(prac)
+
+# %%
